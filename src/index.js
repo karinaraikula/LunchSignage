2522eb62be10a08e47b92743a12016f75f25df45
Saija Temisevä <saijatemiseva@Saija-MacBook-Pro.local>
2022-03-08 16:19:17 +0200

    bussit

--- src/index.js
+++ src/index.js
@@ -104,7 +104,7 @@ const init = () => {
   fetchData(HSLData.apiUrl, {
     method: 'POST',
     headers: { 'Content-Type': 'application/graphql' },
-    body: HSLData.getQueryForNextRidesByStopId(),
+    body: HSLData.getQueryForNextRidesByStopId(2132207),
 
   }).then(response => {
     // TODO: create separate render HSL data functions (in HSLData module maybe?)
@@ -112,14 +112,13 @@ const init = () => {
     const hslContent = document.querySelector('.hsl-data');
     hslContent.innerHTML = ``;
     
-    console.log('hsl data', response.data.stop.stoptimesWithoutPatterns[0]);
     
     for (let i = 0; i < 4; i++) {
     const stop = response.data.stop;
     let time = new Date((stop.stoptimesWithoutPatterns[i].realtimeArrival + stop.stoptimesWithoutPatterns[i].serviceDay) * 1000);
     let localeSpecificTime = time.toLocaleTimeString('fi-FI', { hour: 'numeric', minute: 'numeric' });
    
-    console.log(stop.name, stop.stoptimesWithoutPatterns[i].trip.routeShortName, stop.stoptimesWithoutPatterns[i].headsign, localeSpecificTime);
+    console.log(stop.name, stop.stoptimesWithoutPatterns[i].trip.routeShortName, stop.stoptimesWithoutPatterns[i].headsign,localeSpecificTime);
     
     hslContent.innerHTML += `
     <ul class="hsl-row">
